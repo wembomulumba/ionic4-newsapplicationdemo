44f4eb9f56f2d474e6099ec6fc61bcbebff6794a
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-05-08 18:21:11 +0200

    code review news.service.ts

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import {HttpClient} from '@angular/common/http';
-
+// settomgs to connect to Backend api (fecthing data)
 const API_URL = environment.apiUrl;
 const API_KEY = environment.apiKey;
 @Injectable({
@@ -18,6 +18,7 @@ export class NewsService {
     (`${API_URL}/${url}`);
   }
 
+  // function to retrieve all the news feed
   getNotes() {
     return this.http.get('http://localhost:3000/news/');
   }
